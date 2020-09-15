7c440545fa5e6e2c695445ab881b89b99369dcd9
lushengbo <rain19870225@163.com>
2020-09-15 15:01:32 +0800

    顶点转换

--- facedetectlibrary/src/main/java/com/cgfay/facedetect/engine/FaceTracker.java
+++ facedetectlibrary/src/main/java/com/cgfay/facedetect/engine/FaceTracker.java
@@ -85,9 +85,6 @@ public final class FaceTracker {
     /**
      * 检测人脸
      *
-     * @param data
-     * @param width
-     * @param height
      */
     public void trackFace(byte[] data, int width, int height) {
         synchronized (mSyncFence) {
@@ -360,7 +357,6 @@ public final class FaceTracker {
          * @param data   图像数据， NV21 或者 RGBA格式
          * @param width  图像宽度
          * @param height 图像高度
-         * @return 是否检测成功
          */
         public void trackFace(final byte[] data, final int width, final int height) {
             waitUntilReady();
@@ -433,10 +429,7 @@ public final class FaceTracker {
         /**
          * 检测人脸
          *
-         * @param data   图像数据，预览时为NV21，静态图片则为RGBA格式
-         * @param width  图像宽度
-         * @param height 图像高度
-         * @return 是否检测成功
+         * @param data 图像数据，预览时为NV21，静态图片则为RGBA格式
          */
         private synchronized void internalTrackFace(byte[] data, int width, int height) {
             FaceTrackParam faceTrackParam = FaceTrackParam.getInstance();
@@ -543,13 +536,15 @@ public final class FaceTracker {
                     }
 //
                     // 获取一个人的关键点坐标
-                    if (oneFace.vertexPoints == null || oneFace.vertexPoints.length != face.landmarks.length * 2) {
-                        oneFace.vertexPoints = new float[face.landmarks.length * 2];
+                    if (oneFace.vertexPoints == null || oneFace.vertexPoints.length != face.landmarks.length) {
+                        oneFace.vertexPoints = new float[face.landmarks.length];
                     }
                     for (int i = 0; i + 1 < face.landmarks.length; i += 2) {
                         // orientation = 0、3 表示竖屏，1、2 表示横屏
                         float x = ((face.landmarks[i] / (float) height) * 2) - 1;
                         float y = ((face.landmarks[i + 1] / (float) width) * 2) - 1;
+//                        float x = face.landmarks[i];
+//                        float y = face.landmarks[i + 1];
                         float[] point = new float[]{x, -y};
                         if (orientation == 1) {
                             if (faceTrackParam.previewTrack && faceTrackParam.isBackCamera) {
@@ -572,16 +567,17 @@ public final class FaceTracker {
                             point[1] = y;
                         }
                         // 顶点坐标
+                        int vertexIndex = transform(i / 2);
                         if (faceTrackParam.previewTrack) {
                             if (faceTrackParam.isBackCamera) {
-                                oneFace.vertexPoints[2 * i] = point[0];
+                                oneFace.vertexPoints[vertexIndex * 2] = point[0];
                             } else {
-                                oneFace.vertexPoints[2 * i] = -point[0];
+                                oneFace.vertexPoints[vertexIndex * 2] = -point[0];
                             }
                         } else { // 非预览状态下，左右不需要翻转
-                            oneFace.vertexPoints[2 * i] = point[0];
+                            oneFace.vertexPoints[vertexIndex * 2] = point[0];
                         }
-                        oneFace.vertexPoints[2 * i + 1] = point[1];
+                        oneFace.vertexPoints[vertexIndex * 2 + 1] = point[1];
                     }
                     // 插入人脸对象
                     LandmarkEngine.getInstance().putOneFace(index, oneFace);
@@ -592,6 +588,225 @@ public final class FaceTracker {
             // 检测完成回调
             if (faceTrackParam.trackerCallback != null) {
                 faceTrackParam.trackerCallback.onTrackingFinish();
+            }
+        }
+
+        private int transform(int i) {
+            switch (i) {
+                case 0:
+                    return 16;
+                case 1:
+                    return 53;
+                case 2:
+                    return 101;
+                case 3:
+                    return 73;
+                case 4:
+                    return 91;
+                case 5:
+                    return 6;
+                case 6:
+                    return 5;
+                case 7:
+                    return 4;
+                case 8:
+                    return 3;
+                case 9:
+                    return 2;
+                case 10:
+                    return 1;
+                case 11:
+                    return 0;
+                case 12:
+                    return 57;
+                case 13:
+                    return 32;
+                case 14:
+                    return 30;
+                case 15:
+                    return 31;
+                case 16:
+                    return 28;
+                case 17:
+                    return 29;
+                case 18:
+                    return 26;
+                case 19:
+                    return 33;
+                case 20:
+                    return 61;
+                case 21:
+                    return 43;
+                case 22:
+                    return 45;
+                case 23:
+                    return 44;
+                case 24:
+                    return 38;
+                case 25:
+                    return 99;
+                case 26:
+                    return 88;
+                case 27:
+                    return 58;
+                case 28:
+                    return 37;
+                case 29:
+                    return 35;
+                case 30:
+                    return 92;
+                case 31:
+                    return 82;
+                case 32:
+                    return 93;
+                case 33:
+                    return 89;
+                case 34:
+                    return 72;
+                case 35:
+                    return 78;
+                case 36:
+                    return 98;
+                case 37:
+                    return 85;
+                case 38:
+                    return 87;
+                case 39:
+                    return 86;
+                case 40:
+                    return 97;
+                case 41:
+                    return 75;
+                case 42:
+                    return 100;
+                case 43:
+                    return 76;
+                case 44:
+                    return 68;
+                case 45:
+                    return 84;
+                case 46:
+                    return 49;
+                case 47:
+                    return 62;
+                case 48:
+                    return 71;
+                case 49:
+                    return 24;
+                case 50:
+                    return 90;
+                case 51:
+                    return 63;
+                case 52:
+                    return 77;
+                case 53:
+                    return 54;
+                case 54:
+                    return 69;
+                case 55:
+                    return 104;
+                case 56:
+                    return 25;
+                case 57:
+                    return 12;
+                case 58:
+                    return 66;
+                case 59:
+                    return 55;
+                case 60:
+                    return 67;
+                case 61:
+                    return 96;
+                case 62:
+                    return 64;
+                case 63:
+                    return 103;
+                case 64:
+                    return 94;
+                case 65:
+                    return 95;
+                case 66:
+                    return 8;
+                case 67:
+                    return 56;
+                case 68:
+                    return 27;
+                case 69:
+                    return 46;
+                case 70:
+                    return 40;
+                case 71:
+                    return 70;
+                case 72:
+                    return 74;
+                case 73:
+                    return 39;
+                case 74:
+                    return 42;
+                case 75:
+                    return 41;
+                case 76:
+                    return 15;
+                case 77:
+                    return 14;
+                case 78:
+                    return 13;
+                case 79:
+                    return 36;
+                case 80:
+                    return 11;
+                case 81:
+                    return 10;
+                case 82:
+                    return 9;
+                case 83:
+                    return 65;
+                case 84:
+                    return 34;
+                case 85:
+                    return 60;
+                case 86:
+                    return 51;
+                case 87:
+                    return 50;
+                case 88:
+                    return 47;
+                case 89:
+                    return 48;
+                case 90:
+                    return 80;
+                case 91:
+                    return 79;
+                case 92:
+                    return 81;
+                case 93:
+                    return 83;
+                case 94:
+                    return 52;
+                case 95:
+                    return 18;
+                case 96:
+                    return 19;
+                case 97:
+                    return 17;
+                case 98:
+                    return 22;
+                case 99:
+                    return 23;
+                case 100:
+                    return 20;
+                case 101:
+                    return 21;
+                case 102:
+                    return 7;
+                case 103:
+                    return 102;
+                case 104:
+                    return 59;
+                case 105:
+                    return 105;
+                default:
+                    return -1;
             }
         }
     }
